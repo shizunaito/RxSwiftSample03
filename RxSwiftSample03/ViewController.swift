80759f41336e1b45320ed2f711af3c2b31bf2745
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-14 20:11:11 +0900

    add ViewController UI

--- RxSwiftSample03/ViewController.swift
+++ RxSwiftSample03/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameSearchBar: UISearchBar!
+    @IBOutlet weak var repositoryListTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
