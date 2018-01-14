8ec68eebdda7268f85b0321b81fb6495cdfcfdde
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-14 20:52:32 +0900

    add view

--- RxSwiftSample03/ViewController.swift
+++ RxSwiftSample03/ViewController.swift
@@ -7,15 +7,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import ObjectMapper
+import RxAlamofire
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var nameSearchBar: UISearchBar!
     @IBOutlet weak var repositoryListTableView: UITableView!
 
+    let disposeBag = DisposeBag()
+
+    var repositoryViewModel: RepositoryViewModel!
+
+    var rx_serchBarText: Observable<String> {
+        return nameSearchBar.rx.text
+            .filter { $0 != nil }
+            .map { $0! }
+            .filter { $0.count > 0 }
+            .debounce(0.5, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupRx()
+    }
+
+    func setupRx() {
+        repositoryViewModel = RepositoryViewModel(withNameObservable: rx_serchBarText)
+
+        // tableViewへのデータ渡し
+        repositoryViewModel
+            .rx_repositories
+            .drive(repositoryListTableView.rx.items) { (tableView, i, repository) in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell", for: IndexPath(row: i, section: 0))
+                cell.textLabel?.text = repository.name
+                cell.detailTextLabel?.text = repository.url
+
+                return cell
+            }
+            .disposed(by: disposeBag)
+
+        // データ取得件数0の時のエラー処理
+        repositoryViewModel
+            .rx_repositories
+            .drive(onNext: { repositories in
+                if repositories.count == 0 {
+                    let alert = UIAlertController(title: ":(", message: "No results.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+                    if self.navigationController?.visibleViewController is UIAlertController != true {
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
