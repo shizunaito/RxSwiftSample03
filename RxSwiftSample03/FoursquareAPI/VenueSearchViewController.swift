e607146bb1ae28f79d445bfd50fcd13d33af0541
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-17 22:02:27 +0900

    add viewcontroller UI

---
+++ RxSwiftSample03/FoursquareAPI/VenueSearchViewController.swift
@@ -0,0 +1,41 @@
+//
+//  VenueSearchViewController.swift
+//  RxSwiftSample03
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/01/15.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SafariServices
+
+class VenueSearchViewController: UIViewController {
+
+    @IBOutlet weak var venueSearchBar: UISearchBar!
+    @IBOutlet weak var venueSearchTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
